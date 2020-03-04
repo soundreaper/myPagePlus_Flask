353f1cae20720db849663d4d05fe28ecef4d4079
soundreaper@gmail.com <soundreaper@gmail.com>
2020-03-04 10:07:08 -0800

    add route for favicon

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, url_for, request
+import os
+from flask import Flask, render_template, url_for, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'img/mypage.jpg', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
