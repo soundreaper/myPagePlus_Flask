30df274ec71a7bfec7fe0ef536fe6a9a502c554e
soundreaper <soundreaper@gmail.com>
2020-03-03 23:58:49 -0800

    clean up app.py

--- app.py
+++ app.py
@@ -22,35 +22,5 @@ def register():
     """Page for registering"""
     return render_template('register.html')
 
-@app.route("/weather")
-def weather():
-    location = request.args.get("address")
-    coords = geocode(location)
-
-    req = requests.get("https://api.darksky.net/forecast/"+ okey + "/"+ coords)
-
-    reqJson = req.json()
-
-    weather = {
-        "temp": reqJson["currently"]["temperature"],
-        "humidity": reqJson["currently"]["humidity"],
-        "wind": reqJson["currently"]["windSpeed"],
-        "summary": reqJson["currently"]["summary"],
-        "high": reqJson["daily"]["temperatureHigh"],
-        "low": reqJson["daily"]["temperatureLow"]
-    }
-    message = f"It is currently {weather['temp']} with a high of {weather['high']} and a low of {weather['low']}. "
-
-    return render_template("index.html", weather=weather)
-
-
-def geocode(location):
-    url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ location +'.json?access_token=' + key + '&autocomplete=true'
-
-    res = requests.get(url)
-    latitude, longitude = res.json()["features"][0]["geometry"]["coordinates"]
-
-    return str(latitude + ',' + longitude)
-
 if __name__ == '__main__':
     app.run(debug=True)
